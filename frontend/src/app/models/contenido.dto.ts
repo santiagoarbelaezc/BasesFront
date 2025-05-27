0575a28762952ce6bd83b77c091cb04e046572fd
Stevenz02 <jssmorales51@gmail.com>
2025-05-27 13:28:04 -0500

    Update docu #2
    Upd doc #2

--- frontend/src/app/models/contenido.dto.ts
+++ frontend/src/app/models/contenido.dto.ts
@@ -1,5 +1,8 @@
+// Representa un contenido asociado a una unidad
+// temática dentro del sistema educativo.
 export interface ContenidoDTO {
   id?: number;       // Opcional para crear
   nombre: string;
   unidad_id: number;
 }
+
