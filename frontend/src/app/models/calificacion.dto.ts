0ed0916eefaf992b6ef66b6f558de67ba4b83972
Stevenz02 <jssmorales51@gmail.com>
2025-05-27 13:25:00 -0500

    Update documentación
    Update documentacion

--- frontend/src/app/models/calificacion.dto.ts
+++ frontend/src/app/models/calificacion.dto.ts
@@ -1,13 +1,14 @@
 import { ExamenDTO } from "./exam.dto";
 import { UsuarioDTO } from "./usuario.dto";
 
+// Representa la calificación obtenida
+//  por un usuario en un examen, incluyendo fecha, nota y tiempos de ejecución.
 export interface CalificacionDTO {
-  
   fecha?: Date;
   horaInicio?: Date;
   horaFin?: Date;
   porcentaje?: number;
   examen: ExamenDTO;
   nota: number;
-  
 }
+
