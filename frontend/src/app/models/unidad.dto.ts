ae1bf209741e5a6e8f6f49d47fde6a0fcaf89774
Stevenz02 <jssmorales51@gmail.com>
2025-05-27 13:41:29 -0500

    Update docu 9
    Upt doc 9

--- frontend/src/app/models/unidad.dto.ts
+++ frontend/src/app/models/unidad.dto.ts
@@ -1,3 +1,4 @@
+// Representa una unidad temática perteneciente a un curso académico.
 export interface UnidadDTO {
   id?: number;
   nombre: string;
--- frontend/src/app/models/usuarioGrupo.dto.ts
+++ frontend/src/app/models/usuarioGrupo.dto.ts
@@ -1,3 +1,5 @@
+// Representa la relación entre un usuario y un 
+// grupo académico dentro del sistema.
 export interface UsuarioGrupoDTO {
   usuario_grupo_id?: number;
   usuarioId: number;
