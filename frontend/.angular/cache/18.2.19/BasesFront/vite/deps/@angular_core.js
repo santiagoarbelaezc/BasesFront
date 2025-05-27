fef4b42ecf5cd63408fc67ef129cc080eb4a0d29
Stevenz02 <jssmorales51@gmail.com>
2025-05-26 23:45:09 -0500

    Creacion vista estudiantes
    Se crea la vista de estudiantes en el panel del profesor utilizando el endpoint para llamar a los usuarios por id = 3 = estudiantes

--- frontend/.angular/cache/18.2.19/BasesFront/vite/deps/@angular_core.js
+++ frontend/.angular/cache/18.2.19/BasesFront/vite/deps/@angular_core.js
@@ -462,8 +462,8 @@ import {
   ɵɵviewQuery,
   ɵɵviewQuerySignal
 } from "./chunk-56G56EPG.js";
-import "./chunk-PEBH6BBU.js";
 import "./chunk-WPM5VTLQ.js";
+import "./chunk-PEBH6BBU.js";
 import "./chunk-4S3KYZTJ.js";
 import "./chunk-TXDUYLVM.js";
 export {
--- frontend/.angular/cache/18.2.19/BasesFront/vite/deps/@angular_forms.js
+++ frontend/.angular/cache/18.2.19/BasesFront/vite/deps/@angular_forms.js
@@ -42,10 +42,10 @@ import {
   ɵɵgetInheritedFactory,
   ɵɵlistener
 } from "./chunk-56G56EPG.js";
-import "./chunk-PEBH6BBU.js";
 import {
   forkJoin
 } from "./chunk-WPM5VTLQ.js";
+import "./chunk-PEBH6BBU.js";
 import {
   Subject,
   from,
