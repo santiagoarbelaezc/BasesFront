0227792daed964fbb8e9a8450b15aba0949f8398
santiagoarbelaezc <santiago.arbelaezc@uqvirtual.edu.co>
2025-05-27 10:07:04 -0500

    nuevos cambios en el horario service

--- frontend/src/app/services/horario.service.ts
+++ frontend/src/app/services/horario.service.ts
@@ -1,15 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { HorarioDTO } from '../models/horario.dto';
 
-export interface Horario {
-  horario_id?: number;
-  grupo_id: number;
-  dia: string;
-  hora_inicio: string;
-  hora_fin: string;
-  aula: string;
-}
+
 
 @Injectable({
   providedIn: 'root'
@@ -19,20 +13,25 @@ export class HorarioService {
 
   constructor(private http: HttpClient) {}
 
-  crearHorario(horario: Horario): Observable<any> {
+  crearHorario(horario: HorarioDTO): Observable<any> {
     return this.http.post(this.apiUrl, horario);
   }
 
-  obtenerHorariosPorGrupo(grupo_id: number): Observable<Horario[]> {
-    return this.http.get<Horario[]>(`${this.apiUrl}/${grupo_id}`);
+  obtenerHorariosPorGrupo(grupo_id: number): Observable<HorarioDTO[]> {
+    return this.http.get<HorarioDTO[]>(`${this.apiUrl}/${grupo_id}`);
   }
 
-  actualizarHorario(id: number, horario: Partial<Horario>): Observable<any> {
+  actualizarHorario(id: number, horario: Partial<HorarioDTO>): Observable<any> {
     return this.http.put(`${this.apiUrl}/${id}`, horario);
   }
 
   eliminarHorario(id: number): Observable<any> {
     return this.http.delete(`${this.apiUrl}/${id}`);
   }
-  
+
+obtenerTodosLosHorarios(): Observable<HorarioDTO[]> {
+  return this.http.get<HorarioDTO[]>(`${this.apiUrl}`);
+}
+
+
 }
