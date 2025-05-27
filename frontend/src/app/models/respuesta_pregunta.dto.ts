80a2074da4cb3527ab4034557a93a2dc91445646
Stevenz02 <jssmorales51@gmail.com>
2025-05-27 13:39:56 -0500

    Update docu 8
    Upd doc 8

--- frontend/src/app/models/respuesta_pregunta.dto.ts
+++ frontend/src/app/models/respuesta_pregunta.dto.ts
@@ -1,3 +1,5 @@
+// Representa la respuesta de un estudiante a
+// una pregunta dentro de un examen presentado.
 export interface RespuestaEstudianteDTO {
   id?: number;
   esCorrecta: number;       // o boolean, según cómo manejes el valor
